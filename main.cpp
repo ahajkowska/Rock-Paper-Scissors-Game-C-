4405a21b2f0f6365c7c9d17540e6b89dd66ab5e4
Amelia Hajkowska <ameliahajkowska@gmail.com>
2024-07-09 16:09:31 +0200

    opponent chooses an item

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <ctime> //current time
+#include <cstdlib> //include rand()
 using namespace std;
 
 int main() {
@@ -11,6 +13,13 @@ int main() {
     int playerScore = 0;
     int oppponentScore = 0;
 
+    //rock, paper, scissors (opponent randomly chooses from this)
+    string rps[] = {"rock", "paper", "scissors"};
+
+    //srand = random seed value based on time
+    //generates random seed based on a current time -> because the current time changes we can guarantee a random number that is different every time we run the program
+    srand((unsigned int)time(NULL));
+
     while (true) {
         cout << "Enter rock, paper, or scissors:";
         getline(cin, player); //gets entire line and assign it to the string player
@@ -18,5 +27,10 @@ int main() {
         if (player != "rock" && player != "paper" && player != "scissors") {
             break; //terminates the while loop
         }
+
+        //assign opponent --> give us a random index to choose from
+        opponent = rps[rand() % 3]; //rand() = generates random number
+
+        cout << "Opponent chooses " << opponent << endl;
     }
 }
